b5ad2c289a0843be5019a2e4c2d836611aa1235f
Nurjanbek A <nurjanbek2196@gmail.com>
2023-08-13 17:23:32 +0500

    all fragments done

--- app/src/main/java/com/example/braingame/presentations/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/braingame/presentations/ChooseLevelFragment.kt
@@ -5,14 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.braingame.R
+import com.example.braingame.databinding.FragmentChooseLevelBinding
 
 class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = (_binding ?: RuntimeException("FragmentChooseLevelBinding == null")) as FragmentChooseLevelBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonLevelEasy.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
