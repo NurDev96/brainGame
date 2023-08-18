86e345fcdb2a59f9d13586d3c23de6503df92511
Nurjanbek A <nurjanbek2196@gmail.com>
2023-08-18 11:53:32 +0500

    Serializable

--- app/src/main/java/com/example/braingame/domain/entity/Level.kt
+++ app/src/main/java/com/example/braingame/domain/entity/Level.kt
@@ -1,6 +1,9 @@
 package com.example.braingame.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-enum class Level {
+@Parcelize
+enum class Level : Parcelable {
     TEST, NORMAL, EASY, HARD
 }
